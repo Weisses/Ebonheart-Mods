278ad20849d2d74ed2dbb465b68fe50f012b0aea
Weisses <david.l.aiello@gmail.com>
2016-09-06 14:40:20 -0400

    Prerelease 1.3.2a
    More cleanup.  (Past comment I forgot to put in.)

--- ViesCraft/1.10.2/src/main/java/com/viesis/viescraft/api/compat/jei/BlankModPlugin.java
+++ ViesCraft/1.10.2/src/main/java/com/viesis/viescraft/api/compat/jei/BlankModPlugin.java
@@ -2,14 +2,15 @@ package com.viesis.viescraft.api.compat.jei;
 
 import javax.annotation.Nonnull;
 
-public abstract class BlankModPlugin implements IModPlugin {
-	@Override
-	public void register(@Nonnull IModRegistry registry) {
+public abstract class BlankModPlugin //implements IModPlugin 
+{
+	//@Override
+	//public void register(@Nonnull IModRegistry registry) {
 
-	}
+	//}
 
-	@Override
-	public void onRuntimeAvailable(@Nonnull IJeiRuntime jeiRuntime) {
+	//@Override
+	//public void onRuntimeAvailable(@Nonnull IJeiRuntime jeiRuntime) {
 
-	}
+	//}
 }
