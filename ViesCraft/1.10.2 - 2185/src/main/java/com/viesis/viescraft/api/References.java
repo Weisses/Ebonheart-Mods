b5cbf2edb7c0531d0db92c5288bbd728e3c59e7a
Weisses <david.l.aiello@gmail.com>
2017-12-03 15:28:39 -0500

    Setting 1.10.2 to be final.

--- ViesCraft/1.10.2 - 2185/src/main/java/com/viesis/viescraft/api/References.java
+++ ViesCraft/1.10.2 - 2185/src/main/java/com/viesis/viescraft/api/References.java
@@ -15,7 +15,7 @@ public class References {
 	public static final String MOD_ID = "vc";
 	public static final String MOD_ID_COLON = MOD_ID + ":";
 	public static final String MOD_NAME = "ViesCraft";
-	public static final String MOD_VERSION = "5.5.0";
+	public static final String MOD_VERSION = "5.5.0-final";
 	public static final String CONFIG_GUI = "com.viesis.viescraft.api.gui.VCGuiFactory";
 	public static final String CLIENT_PROXY_CLASS = "com.viesis.viescraft.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.viesis.viescraft.proxy.ServerProxy";
